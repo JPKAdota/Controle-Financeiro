a01c1e8026767bd11d1b7b08b11df054e5e1b944
JPKAdota <joaokadota@gmail.com>
2025-09-24 17:45:16 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# Crie um arquivo app.py básico para testar
+import streamlit as st
+
+st.set_page_config(page_title="Controle Financeiro", layout="wide")
+st.title("💰 Meu Controle Financeiro Inteligente")
+st.sidebar.title("Navegação")
